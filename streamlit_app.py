ede4c03f54d906a08109f80fd5564bcdaf5457cb
Sushant Mittal <124278879+smittal1205@users.noreply.github.com>
2024-09-12 10:10:59 +0000

    Modified streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Here's our first table:")
-st.write(pd.DataFrame(
+st.title("Sushant Grocessors 😃😂🏋️🍷")
+st.write("Stock:")
+stock = pd.DataFrame(
     {
         "col1":['Apple', 'Banana', 'Pear', 'Orannge'],
         "col2":[10, 20, 40, 20]
     }
-))
-st.line_chart(pd.DataFrame(
-    {
-        "col1":['Apple', 'Banana', 'Pear', 'Orannge'],
-        "col2":[10, 20, 40, 20]
-    }
-))
+)
+st.write(stock)
+
+option = st.selectbox(
+    'What would you like to buy?',
+    stock.col2.to_list()
+)
+
+st.write("You selected:", option)
