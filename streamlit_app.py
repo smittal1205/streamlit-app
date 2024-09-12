f36ae7758d9a14cef2de96e8b231c7f5850b2298
Sushant Mittal <124278879+smittal1205@users.noreply.github.com>
2024-09-12 10:04:38 +0000

    Modified streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Here's our first table:")
+st.write(pd.DataFrame(
+    {
+        "col1":['Apple', 'Banana', 'Pear', 'Orannge'],
+        "col2":[10, 20, 40, 20]
+    }
+))
